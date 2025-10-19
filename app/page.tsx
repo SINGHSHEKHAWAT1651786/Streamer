a63471a9768e88697b6eecfcf11f9c0c93d35e80
Govind Shekhawat <SINGHSHEKHAWAT1651786@GMAIL.COM>
2025-10-19 22:11:08 +0530

    Added button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <p>Hello  Streamer</p>
+   <Button>
+    Click me
+   </Button>
   );
 }
