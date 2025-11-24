3b91a1fab0abaf3086d3b9214aff67d163b23e39
GOVIND SINGH SHEKHAWAT <128773189+SINGHSHEKHAWAT1651786@users.noreply.github.com>
2025-11-24 21:15:51 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* configuration options here, any type of configuration done here */
+  /* It import the configuration of Next */
 };
 
 export default nextConfig;
