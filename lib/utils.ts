1f0821b151f7b1a9d8ea71fa09533585c73c6352
GOVIND SINGH SHEKHAWAT <128773189+SINGHSHEKHAWAT1651786@users.noreply.github.com>
2025-12-25 20:01:33 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,1 @@
-// It merge the value of the  utils and clsx
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
