f155bfcb059c040fa7c5aa99987cb912be87882a
GOVIND SINGH SHEKHAWAT <128773189+SINGHSHEKHAWAT1651786@users.noreply.github.com>
2025-12-25 20:01:46 +0530

    Implement 'cn' function for class name merging
    Add a utility function 'cn' to merge class names.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,8 @@
+// It merge the value of the  utils and clsx
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
